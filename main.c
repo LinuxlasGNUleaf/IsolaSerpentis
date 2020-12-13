e33011ad9b179768fefeede1ea88c8ad71098c0c
Jakob Weiß <jakob.l.weiss@gmx.de>
2020-12-13 22:55:06 +0100

    implement the intro

--- main.c
+++ main.c
@@ -1,45 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "texts.h"
+
 #ifdef _WIN32
 #include <windows.h>
 #else
 #include <unistd.h>
 #endif
 
-#include <stdio.h>
-#include <stdlib.h>
+const int max_line_length = 120;
+const int standard_reading_tempo = 14;
 
-char title[] = "d888888b .d8888.  .d88b.  db       .d8b.      .d8888. d88888b d8888b. d8888b. d88888b d8b   db d888888b d888888b .d8888. \n\
-  `88'   88'  YP .8P  Y8. 88      d8' `8b     88'  YP 88'     88  `8D 88  `8D 88'     888o  88 `~~88~~'   `88'   88'  YP \n\
-   88    `8bo.   88    88 88      88ooo88     `8bo.   88ooooo 88oobY' 88oodD' 88ooooo 88V8o 88    88       88    `8bo.   \n\
-   88      `Y8b. 88    88 88      88~~~88       `Y8b. 88~~~~~ 88`8b   88~~~   88~~~~~ 88 V8o88    88       88      `Y8b. \n\
-  .88.   db   8D `8b  d8' 88booo. 88   88     db   8D 88.     88 `88. 88      88.     88  V888    88      .88.   db   8D \n\
-Y888888P `8888Y'  `Y88P'  Y88888P YP   YP     `8888Y' Y88888P 88   YD 88      Y88888P VP   V8P    YP    Y888888P `8888Y'\n\n\
-//=============================\\\\\n\
-||Written in BASIC by:         ||\n\
-||Daniel Weiß \t Andreas Enge  ||\n\
-\\\\=============================//\n\n\
-//==================================\\\\\n\
-||Reconstructed and ported to C by: ||\n\
-||Jakob Weiß                        ||\n\
-\\\\==================================//\n";
+void os_wait(int ms)
+{
+#ifdef _WIN32
+    Sleep(ms);
+#else
+    usleep(ms*1000);
+#endif
+}
+
+void fancy_print(char str[], int len, int ms, bool auto_line_break)
+{
+    int last_line_break = 0;
+    int next_possible_break = 0;
+    for (int i = 0; i<len; i++)
+    {
+        if (auto_line_break && i >= next_possible_break && i < len-1)
+        {
+            if (i > last_line_break+max_line_length)
+            {
+                printf("\n");
+                fflush(stdout);
+                last_line_break = i;
+                if (str[i] == ' ')
+                    i += 1;
+            }
+            else
+            {
+                for (int k = i; k < len-1; k++)
+                {
+                    if (str[k] == ' ')
+                    {
+                        next_possible_break = k;
+                        break;
+                    }
+                }
+            }
+        }
+        printf("%c",str[i]);
+        fflush(stdout);
+        int breaker_stat = 0;
+        for (int k = 0; k < sizeof(breakers); k++)
+        {
+            if (str[i] == breakers[k])
+                breaker_stat = 2;
+        }
+        if (breaker_stat == 0)
+        {
+            for (int k = 0; k < sizeof(half_breakers); k++)
+            {
+                if (str[i] == half_breakers[k])
+                    breaker_stat = 1;
+            }
+        }
+        if (auto_line_break && breaker_stat != 0)
+        {
+            if (breaker_stat == 1)
+                os_wait(ms*10);
+            else if (breaker_stat == 2)
+                os_wait(ms*35);
+            continue;
+        }
+        else
+            os_wait(ms);
+    }
+}
 
 void start_seq()
 {
-    for (int i=0; i<sizeof(title); i++)
-    {
-        printf("%c",title[i]);
-        if (title[i]=='\n')
-        {
-#ifdef _WIN32
-            Sleep(500);
-#else
-            usleep(200*1000);
-#endif
-        }
-    }
-    return 0;
+    fancy_print(title, sizeof(title), 4, false);
+    printf("Drücke ENTER, um zu starten.");
+    getchar();
+    printf("\n\n");
+    fancy_print(intro1, sizeof(intro1), standard_reading_tempo, true);
+    printf("Drücke ENTER, um weiterzufahren.");
+    getchar();
+    printf("\n\n");
+    fancy_print(intro2, sizeof(intro2), standard_reading_tempo, true);
 }
 
 int main()
 {
     start_seq();
+    return 0;
 }
---
+++ texts.h
@@ -0,0 +1,20 @@
+char title[] = "d888888b .d8888.  .d88b.  db       .d8b.      .d8888. d88888b d8888b. d8888b. d88888b d8b   db d888888b d888888b .d8888. \n\
+  `88'   88'  YP .8P  Y8. 88      d8' `8b     88'  YP 88'     88  `8D 88  `8D 88'     888o  88 `~~88~~'   `88'   88'  YP \n\
+   88    `8bo.   88    88 88      88ooo88     `8bo.   88ooooo 88oobY' 88oodD' 88ooooo 88V8o 88    88       88    `8bo.   \n\
+   88      `Y8b. 88    88 88      88~~~88       `Y8b. 88~~~~~ 88`8b   88~~~   88~~~~~ 88 V8o88    88       88      `Y8b. \n\
+  .88.   db   8D `8b  d8' 88booo. 88   88     db   8D 88.     88 `88. 88      88.     88  V888    88      .88.   db   8D \n\
+Y888888P `8888Y'  `Y88P'  Y88888P YP   YP     `8888Y' Y88888P 88   YD 88      Y88888P VP   V8P    YP    Y888888P `8888Y'\n\n\
+//=============================\\\\\n\
+||Written in BASIC by:         ||\n\
+||Daniel Weiß \t Andreas Enge  ||\n\
+\\\\=============================//\n\n\
+//==================================\\\\\n\
+||Reconstructed and ported to C by: ||\n\
+||Jakob Weiß                        ||\n\
+\\\\==================================//\n";
+
+char intro1[] = "Du befindest dich an Bord deines Fischerbootes auf hoher See. Während deine zum Trocknen aufgehängte Netze in der Abendbrise schaukeln, liegst du im Bug deines Schiffes, schaust in die untergehende Sonne und denkst an dein zu Hause zurückgebliebene Familie. Die Wellen schlagen leise an den Schiffsrumpf. Allmählich fühlst du, wie du von einer wohligen Müdigkeit umfangen wirst. In einen tiefen, traumlosen Schlaf hinübergleitend, hörst du noch das leise Säuseln des Windes…\n";
+char intro2[] = "Du versuchst deine Augen zu öffnen, musst sie aber sofort geblendet wieder schließen. Du fühlst dich wie zerschlagen. Die Augen mit der Hand bedeckend blinzelst du in die gleißende Sonne und versuchst mühsam, dich aufzurappeln. Taumelnd kommst du wieder auf die Beine und blickst dich verstört um: Du stehst auf einem weiten Strand der aus Myriaden von kleinen violett glänzenden Muschelschalen besteht. Er setzt sich nach Osten und Westen fort. Einige zehn Meter im Süden siehst du das spiegelglatte Meer. Nördlich von dir ragt die dichtgeschlossene Wand eines türkisgrünen Waldes zum glühenden Himmel auf. Nur an einigen Stellen scheinen die Lianen einen schmalen Durchgang zu gewähren.\n";
+
+char half_breakers[] = ",;";
+char breakers[] = ".:…";
